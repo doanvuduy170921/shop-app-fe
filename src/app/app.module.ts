df8f77d43b0eab78e58a0301e7868733fb70b911
Doan Vu Duy <doanvuduyndh@gmail.com>
2024-08-15 15:47:04 +0700

    add order and order confirm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-
     HomeComponent,
      HeaderComponent,
-     FooterComponent
+     FooterComponent,
+     DetailProductComponent,
+     OrderComponent,
+     OrderConfirmComponent,
+     LoginComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [//DetailProductComponent,
+                //OrderComponent,
+    OrderConfirmComponent]
 })
 export class AppModule { }
